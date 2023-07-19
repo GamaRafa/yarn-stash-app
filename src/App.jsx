ddf2fabcfbcaa2379014bac669f069b8889ca8ea
Rafael Gama <rafa.gama.em7@gmail.com>
2023-07-19 17:48:34 -0300

    Components created. Folder structure organized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,70 @@
 import { useState } from 'react'
 //import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Home } from './Home/Home.jsx'
+// import { AdicionarFio } from './Adicionar-fio/Adicionar-fio.jsx'
+// import { ConsultarFio } from './Consultar-fio/Consultar-fio.jsx'
+// import { VerLista } from './Ver-lista/Ver-lista.jsx'
 
-  return
+export function App() {
+    const [ showHome, setShowHome ] = useState(true)
+/*
+    const [ showAdicionarFio, setShowAdicionarFio ] = useState(false)
+    const [ showConsultarFio, setShowConsultarFio ] = useState(false)
+    const [ showVerLista, setShowVerLista] = useState(false) */
+
+/*     const handleClickHome = () => {
+        setShowHome(true)
+        setShowAdicionarFio(false)
+        setShowConsultarFio(false)
+        setShowVerLista(false)
+    }
+
+    const handleClickAdicionarFio = () => {
+        setShowHome(false)
+        setShowAdicionarFio(true)
+        setShowConsultarFio(false)
+        setShowVerLista(false)
+    }
+
+    const handleClickConsultarFio = () => {
+        setShowHome(false)
+        setShowAdicionarFio(false)
+        setShowConsultarFio(true)
+        setShowVerLista(false)
+    }
+
+    const handleClickVerLista = () => {
+        setShowHome(false)
+        setShowAdicionarFio(false)
+        setShowConsultarFio(false)
+        setShowVerLista(true)
+    } */
+
+    return(
+      <div className="app">
+        <div className="navbar">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Adicionar fio</a>
+            </li>
+            <li>
+              <a href="#">Consultar fio</a>
+            </li>
+            <li>
+              <a href="#">Ver lista completa</a>
+            </li>
+          </ul>
+        </div>
+        <div className="content">
+          { showHome && <Home />}
+{/*       { showAdicionarFio && <AdicionarFio /> }
+          { showConsultarFio && <ConsultarFio/>  }
+          { showVerLista && <VerLista/>  } */}
+        </div>
+      </div>
+    )
 }
-
-export default App
---
+++ src/Home/Home.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export function Home(){
+    return(
+        <div>Hello! I'm home</div>
+    )
+}
