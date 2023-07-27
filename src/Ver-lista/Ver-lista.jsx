6790ff383f4628175f8b78902c41cb36d1ef9b66
Rafael Gama <rafa.gama.em7@gmail.com>
2023-07-27 15:05:49 -0300

    Iteration tru response object integrated

--- src/Ver-lista/Ver-lista.jsx
+++ src/Ver-lista/Ver-lista.jsx
@@ -8,12 +8,37 @@ function getLista(){
     .then(response => response.json())
     //.then(data => console.log(data))
     .then(data => {
-        nome.innerHTML = `Nome: ${data[0].nome}`
-        marca.innerHTML = `Marca: ${data[0].marca}`
-        material.innerText = `Material: ${data[0].material}`
-        tex.innerText = `TEX: ${data[0].tex}`
-        cor.innerText = `Cor: ${data[0].cor}`
-        quantidade.innerText = `Quantidade: ${data[0].quantidade}`
+        for (let fio in data){
+            let divFio = document.createElement('div')
+            document.getElementById("listDiv").appendChild(divFio).className += 'fio';
+
+            let nome = document.createElement('p')
+            nome.innerHTML = `<strong>Nome:</strong> ${data[fio].nome}`
+            divFio.appendChild(nome)
+
+            let marca = document.createElement('p')
+            marca.innerHTML = `<strong>Marca:</strong> ${data[fio].marca}`
+            divFio.appendChild(marca)
+
+            let material = document.createElement('p')
+            material.innerHTML = `<strong>Material:</strong> ${data[fio].material}`
+            divFio.appendChild(material)
+
+            let tex = document.createElement('p')
+            tex.innerHTML = `<strong>TEX:</strong> ${data[fio].tex}`
+            divFio.appendChild(tex)
+
+            let cor = document.createElement('p')
+            cor.innerHTML = `<strong>Cor:</strong> ${data[fio].cor}`
+            divFio.appendChild(cor)
+
+            let quantidade = document.createElement('p')
+            quantidade.innerHTML = `<strong>Quantidade:</strong> ${data[fio].quantidade}`
+            divFio.appendChild(quantidade)
+
+            let hr = document.createElement('hr')
+            divFio.appendChild(hr);
+        }
     })
     .catch(error => console.error(error))
 }
@@ -25,14 +50,6 @@ export function VerLista(){
             <h2>Lista de fios</h2>
             <button id='btnLista' onClick={getLista}>Ver lista</button>
             <div id="listDiv">
-                <div className="fio">
-                    <p id="nome"></p>
-                    <p id="marca"></p>
-                    <p id="material"></p>
-                    <p id="tex"></p>
-                    <p id="cor"></p>
-                    <p id="quantidade"></p>
-                </div>
             </div>
         </div>
     )
