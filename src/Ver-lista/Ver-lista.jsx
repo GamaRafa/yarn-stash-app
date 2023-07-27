c8607122b9a0b845632953b5e5133402c509aa0a
Rafael Gama <rafa.gama.em7@gmail.com>
2023-07-26 23:28:07 -0300

    Worked on the front-end, created the route to fetch all the data and show in the HTML.

--- src/Ver-lista/Ver-lista.jsx
+++ src/Ver-lista/Ver-lista.jsx
@@ -1,7 +1,39 @@
 import { useState } from "react";
+import './Ver-lista.css'
+
+const url = "http://localhost:5555/"
+
+function getLista(){
+    fetch(url)
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(data => {
+        nome.innerHTML = `${data[0].nome}<br>`
+        marca.innerHTML = `<br>${data[0].marca}`
+        material.innerText = data[0].material
+        tex.innerText = data[0].tex
+        cor.innerText = data[0].cor
+        quantidade.innerText = data[0].quantidade
+    })
+    .catch(error => console.error(error))
+}
+
 
 export function VerLista(){
     return(
-        <div>Hello! I'm Ver lista completa</div>
+        <div id="content">
+            <h2>Lista de fios</h2>
+            <button id='btnLista' onClick={getLista}>Ver lista</button>
+            <div id="listDiv">
+                <div className="fio">
+                    <p id="nome"></p>
+                    <p id="marca"></p>
+                    <p id="material"></p>
+                    <p id="tex"></p>
+                    <p id="cor"></p>
+                    <p id="quantidade"></p>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/api.js
+++ src/api.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const sqlite3 = require('sqlite3')
+const cors = require('cors')
 
 const app = express()
 const port = 5555
+
+app.use(cors())
+app.use(express.json())
 
 // Connect to database
 
